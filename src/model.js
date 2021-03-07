d882343b0c0a354dcd582ca5ed1fe1f399ad2730
Alexey <capsmol@gmail.com>
2021-03-07 15:02:44 +0500

    create project and started developing

---
+++ src/model.js
@@ -0,0 +1,24 @@
+import image from 'url:./assets/image.png'
+
+export const model = [
+  {
+    type: 'title',
+    value: 'Hello world from JS!'
+  },
+  {
+    type: 'text',
+    value: 'here we go with some text'
+  },
+  {
+    type: 'colums',
+    value: [
+      '1111111111',
+      '2222222222',
+      '3333333333'
+    ]
+  },
+  {
+    type: 'image',
+    value: image
+  }
+]
